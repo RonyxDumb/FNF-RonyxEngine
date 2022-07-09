5f6b97ac6812ca78944925e7f867e1e384b44940
Ronyx <104029827+RonyxDumb@users.noreply.github.com>
2022-07-09 23:40:30 +0200

    Ios shit bullyng me

---
+++ art/unsed_source/PageState.hx
@@ -0,0 +1,15 @@
+package;
+
+import flixel.ui.Button;
+import flixel.FlxG;
+
+class PageState extends MusicBeatState
+{
+  override function create()
+  {
+  }
+}
+
+function clickplay()
+{
+  FlxG.switchState(new OptionsMenu());
